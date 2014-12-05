9e54a739e1795ae43633abbfd2649a702fa6e835
DaniLebredo <danilebredo@gmail.com>
2014-12-05 13:04:42 +0100

    Commit inicial

---
+++ src/com/hyperion/arcadia/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.hyperion.arcadia.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hyperion.arcadia.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		MainGame.isAndroid=true;
+		initialize(new MainGame(), config);
+	}
+}
