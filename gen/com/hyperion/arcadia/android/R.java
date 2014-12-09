8cbcfe2cf2e0263589ecc7ef3d2bc8567d19b1ce
DaniLebredo <danilebredo@gmail.com>
2014-12-09 13:35:18 +0100

    Añadido soporte a Analytics
    -Modificado Manifest.
    -Modificado MainActivity.
    -Más cosas que no recuerdo.

--- gen/com/hyperion/arcadia/android/R.java
+++ gen/com/hyperion/arcadia/android/R.java
@@ -14,9 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
-        public static final int GdxTheme=0x7f040000;
+        public static final int GdxTheme=0x7f050000;
+    }
+    public static final class xml {
+        public static final int app_tracker=0x7f030000;
     }
 }
--- src/com/hyperion/arcadia/android/AndroidLauncher.java
+++ src/com/hyperion/arcadia/android/AndroidLauncher.java
@@ -1,6 +1,11 @@
 package com.hyperion.arcadia.android;
 
+import java.util.HashMap;
+
 import android.os.Bundle;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -8,10 +13,39 @@ import com.hyperion.arcadia.MainGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		MainGame.isAndroid=true;
+		MainGame.isAndroid = true;
 		initialize(new MainGame(), config);
 	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		EasyTracker.getInstance(this).activityStart(this); // Add this method.
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		EasyTracker.getInstance(this).activityStop(this); // Add this method.
+	}
+
+	public enum TrackerName {
+		APP_TRACKER, // Tracker used only in this app.
+	}
+	HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+
+	synchronized Tracker getTracker(TrackerName trackerId) {
+		if (!mTrackers.containsKey(trackerId)) {
+
+			GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+			Tracker t = analytics.getTracker(""+R.xml.app_tracker);
+			mTrackers.put(trackerId, t);
+
+		}
+		return mTrackers.get(trackerId);
+	}
+
 }
