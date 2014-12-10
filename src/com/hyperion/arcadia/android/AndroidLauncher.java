002d69e5bb66e6bc487a3761921cdfb9d93e405f
DaniLebredo <danilebredo@gmail.com>
2014-12-10 02:05:04 +0100

    Noche infructuosa
    -He intentado sin éxito poner las nuevas texturas a los botones.
    -He añadido el soporte para Google Analytics, sin resultado.
    -Los nuevos mapas funcionan. Muy bonitos, están muy bien hechos. Tengo
    ganas de verlos con soporte para colisiones (formato TMX).

--- src/com/hyperion/arcadia/android/AndroidLauncher.java
+++ src/com/hyperion/arcadia/android/AndroidLauncher.java
@@ -3,12 +3,14 @@ package com.hyperion.arcadia.android;
 import java.util.HashMap;
 
 import android.os.Bundle;
-import com.google.analytics.tracking.android.EasyTracker;
-import com.google.analytics.tracking.android.GoogleAnalytics;
-import com.google.analytics.tracking.android.Tracker;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.GoogleAnalytics;
+import com.google.analytics.tracking.android.Tracker;
+import com.google.android.gms.analytics.HitBuilders;
 import com.hyperion.arcadia.MainGame;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -17,6 +19,15 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		MainGame.isAndroid = true;
+		// Get tracker.
+		Tracker t = getTracker(TrackerName.APP_TRACKER);
+
+		// Set screen name.
+//		t.setScreenName("Android Main Screen");
+
+		// Send a screen view.
+		t.send(new HitBuilders.AppViewBuilder().build());
+		Log.d("Analytics", "Main");
 		initialize(new MainGame(), config);
 	}
 
@@ -24,24 +35,27 @@ public class AndroidLauncher extends AndroidApplication {
 	public void onStart() {
 		super.onStart();
 		EasyTracker.getInstance(this).activityStart(this); // Add this method.
+		Log.d("Analytics", "Start");
 	}
 
 	@Override
 	public void onStop() {
 		super.onStop();
 		EasyTracker.getInstance(this).activityStop(this); // Add this method.
+		Log.d("Analytics", "Stop");
 	}
 
 	public enum TrackerName {
 		APP_TRACKER, // Tracker used only in this app.
 	}
+
 	HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
 
 	synchronized Tracker getTracker(TrackerName trackerId) {
 		if (!mTrackers.containsKey(trackerId)) {
 
 			GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
-			Tracker t = analytics.getTracker(""+R.xml.app_tracker);
+			Tracker t = analytics.getTracker("" + R.xml.app_tracker);
 			mTrackers.put(trackerId, t);
 
 		}
